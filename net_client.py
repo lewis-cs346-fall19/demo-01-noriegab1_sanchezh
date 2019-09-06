3db14d23fa3140a7b2b7ecb82ca0e2afa9842a23
noriegab1 <ben.e.noriega@gmail.com>
2019-09-06 13:58:45 -0700

    message change

--- net_client.py
+++ net_client.py
@@ -12,12 +12,9 @@ file = open('github_lab.txt')
 for line in file:
 	sock.sendall(line.encode())
 
-while(True):
-	try:
-		msg = sock.recv(1024).decode()
-		print(msg)
 
-	except ConnectionAbortedError:
-		sock.close()
+msg = sock.recv(1024).decode()
+print(msg)
+
 
 sock.close()
